41393fd93dfff0c1f2c61646d843a7b835088ac5
abdulmobeen <mobeendev@gmail.com>
2023-07-02 19:53:26 +0300

    added routes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
     <h2>{{ title }}</h2>
+    <router-link to="/">Go to Home</router-link> 
+    <router-link to="/about">Go to About</router-link>
+<br>
+ <router-view></router-view>
+
+
 </template>
 
 
@@ -7,7 +13,7 @@
 export default {
     data() {
         return {
-                title: "App"      
+                title: "Find a coach App"      
         }
     },
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import Home from './Home.vue';
+import About from './About.vue';
+import NotFound from './NotFound.vue';
+import { createRouter, createWebHistory } from 'vue-router';
 
-const app = createApp(App);
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Home },
+      { path: '/about', component: About },
+      { path: '/:notFound(.*)', component: NotFound }
+    ]
+  });
+
+
+  const app = createApp(App);
+app.use(router);
+
+  
 
 app.mount('#app');
