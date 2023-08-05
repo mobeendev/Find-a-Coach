fc91ffda435c0d6543cb6f6eeeb57d4773753d64
abdulmobeen <mobeendev@gmail.com>
2023-08-05 23:41:51 +0300

    google 🔥 firebase integration

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,19 +1,30 @@
 export default {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+    const url = process.env.VUE_APP_DB_URL;
+  
+    const coachData = {
+      firstName: data.first,
+      lastName: data.last,
+      description: data.desc,
+      hourlyRate: data.rate,
+      areas: data.areas
+    };
 
-      registerCoach(context, data) {
+    const response = await fetch(`${url}/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
 
-        const coachData = {
-          id: context.rootGetters.userId,
-          firstName: data.first,
-          lastName: data.last,
-          description: data.desc,
-          hourlyRate: data.rate,
-          areas: data.areas
-        };
+    // const responseData = await response.json();
 
+    if (!response.ok) {
+      // error ...
+    }
 
-        console.log(coachData);
-
-        context.commit('registerCoach', coachData);
-      }
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
+  }
 };
